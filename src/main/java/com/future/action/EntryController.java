7655af69aacc81feb41691104d1d31f82545641c
Nwp <729576944@qq.com>
2017-01-15 10:33:58 +0800

    添加controller

---
+++ src/main/java/com/future/action/EntryController.java
@@ -0,0 +1,15 @@
+package com.future.action;
+
+import com.future.base.BaseController;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by 牛洧鹏 on 2017/1/15.
+ */
+@Controller
+@Scope("prototype")
+@RequestMapping("/entry")
+public class EntryController extends BaseController{
+}
