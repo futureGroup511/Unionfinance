96247e82187426ef671af33f5d6e1cb98151c432
liuyang8091216 <liuyang8091216@126.com>
2017-01-16 10:37:24 +0800

    刚刚开始

--- src/main/java/com/future/action/ExpenditureRecordController.java
+++ src/main/java/com/future/action/ExpenditureRecordController.java
@@ -4,6 +4,7 @@ import com.future.base.BaseController;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by 牛洧鹏 on 2017/1/15.
@@ -12,4 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Scope("prototype")
 @RequestMapping("expenditurerecord")
 public class ExpenditureRecordController extends BaseController {
+
+    @RequestMapping(value="addExpendUI",method = RequestMethod.GET)
+    public void addExpendUI(){
+        System.out.println("我到了");
+    }
 }
